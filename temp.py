7cf03b81273e1b1280be00513750755e1608d4c9
morozner <46761578+morozner@users.noreply.github.com>
2022-06-22 14:30:23 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,10 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#Mor Rozner 2022
 """
-Spyder Editor
+Created on Wed Jun 22 14:14:29 2022
 
-This is a temporary script file.
+@author: morozner
 """
-
-import numpy as np
-import random
-
 
 objects = ['planet','WD','BH','NS','comet','RG','TZA','GCs','galaxy']
 processes = ['GDF','DF','tides','kicks','RR']
@@ -16,16 +12,49 @@ environments = ['GCs','protoplanetary disk','atmospheres']
 categories = [objects, processes,environments]
 categories_names = ['objects', 'processes','environments']
 
-final_idea = ''
 
 
-for i in range(len(categories)):
-    cat = categories[i]
-    rand = random.randint(0, len(cat)-1)
-    final_idea = final_idea+str(categories_names[i])+':'+str(cat[rand])+' '    
+from tkinter import *
+import random 
 
-    
-print(final_idea)
+class Checkbar(Frame):
+   def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+      Frame.__init__(self, parent)
+      self.vars = []
+      for pick in picks:
+         var = IntVar()
+         chk = Checkbutton(self, text=pick, variable=var)
+         chk.pack(side=side, anchor=anchor, expand=YES)
+         self.vars.append(var)
+   def state(self):
+      return map((lambda var: var.get()), self.vars)
+if __name__ == '__main__':
+   root = Tk()
+   lng = Checkbar(root, categories_names)
+ #  tgl = Checkbar(root, ['English','German'])
+   lng.pack(side=TOP,  fill=X)
+#   tgl.pack(side=LEFT)
+   lng.config(relief=GROOVE, bd=2)
+
+   def allstates(): 
+      print(list(lng.state()))
+   Button(root, text='Quit', command=root.quit).pack(side=RIGHT)
+   Button(root, text='Go!', command=allstates).pack(side=RIGHT)
+   root.mainloop()
+   
+   choices = list(lng.state())
+   final_idea = ''
+   
+   for i in range(len(categories)):
+       
+       if choices[i] == 1:
+           cat=categories[i]
+           rand = random.randint(0, len(cat)-1)
+           final_idea = final_idea+str(categories_names[i])+':'+str(cat[rand])+' '
+           
+   print(final_idea)
+           
+
     
     
 
